9aaca51abffabdd228be279bff400b549be86845
Lucas Bandeira <lucastbandeira@gmail.com>
2024-02-10 14:49:54 -0300

    Leads update and delete views

--- djcrm/webapp/urls.py
+++ djcrm/webapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import lead_detail, lead_list, lead_create
+from .views import lead_detail, lead_list, lead_create, lead_update, lead_delete
 
 app_name = "webapp"
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", lead_list),
     path("create/", lead_create),
     path("<int:pk>", lead_detail),
+    path("<int:pk>/update/", lead_update),
+    path("<int:pk>/delete/", lead_delete),
 ]
--- djcrm/webapp/views.py
+++ djcrm/webapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Lead, Agent
-from .forms import LeadForm
+from .models import Lead
+from .forms import LeadModelForm
 
 
 def lead_list(request):
@@ -16,17 +16,29 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == "POST":
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data["first_name"]
-            last_name = form.cleaned_data["last_name"]
-            age = form.cleaned_data["age"]
-            agent = Agent.objects.first()
-            Lead.objects.create(
-                first_name=first_name, last_name=last_name, age=age, agent=agent
-            )
+            form.save()
             return redirect("/webapp")
     context = {"form": form}
     return render(request, "webapp/lead_create.html", context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {"form": form, "lead": lead}
+    return render(request, "webapp/lead_update.html", context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/webapp")
